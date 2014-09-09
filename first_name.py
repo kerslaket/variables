764c892d886555ed75f344368917aed488a3b1a9
kerslaket <44108@longroad.ac.uk>
2014-09-09 11:04:25 +0100

    AS Computing First Program

---
+++ first_name.py
@@ -0,0 +1,7 @@
+#Toby Kerslake
+#09-09-2014
+#Excercise 1.2 - first program
+
+first_name = input("What is your first name:")
+print(first_name)#prints variable first_name
+print("Hi {0}!".format(first_name)) #substitutes first_name into {0}.

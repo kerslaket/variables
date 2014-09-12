ad3b445747ba316496566fef9dd6cb26b3870667
kerslaket <44108@longroad.ac.uk>
2014-09-12 16:06:01 +0100

    Variable Improvement stretch and challenge excercise one

---
+++ variable_improvemnt_stretch_and_challenge_excercise_one.py
@@ -0,0 +1,12 @@
+#Toby Kerslake
+#12-09-2014
+#Variable Improvement Stretch and Challenge Excercise 1
+
+length = float(input("Please enter the length of your garden"))
+width = float(input("Please enter the width of your garden"))
+
+gardenArea = length * width
+
+gardenPerimeter = (length * 2) + (width * 2)
+
+

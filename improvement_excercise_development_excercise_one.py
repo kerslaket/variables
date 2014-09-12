cd5e52c1ac3833c0d49545849c7dd4fee2497d24
kerslaket <44108@longroad.ac.uk>
2014-09-12 15:53:20 +0100

    Improvemnt Excercise Development Excercise one

---
+++ improvement_excercise_development_excercise_one.py
@@ -0,0 +1,11 @@
+#Toby Kerslake
+#12-09-2014
+#Variable Improvement Develpoment Excercise 1
+
+number1 = float(input("Please enter your first number: "))
+number2 = float(input("please enter your second number: "))
+
+divResult = number1 // number2
+modResult = number1 % number2
+
+print("{0} divided by {1} equals {2} reaminder {3}".format(number1,number2,divResult,modResult))

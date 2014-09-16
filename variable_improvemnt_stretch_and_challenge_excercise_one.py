08a8074ce279c7c154d1a27ac303a93f404e1f4f
kerslaket <44108@longroad.ac.uk>
2014-09-16 11:09:22 +0100

    Variable improvement stretch etc. improved

--- variable_improvemnt_stretch_and_challenge_excercise_one.py
+++ variable_improvemnt_stretch_and_challenge_excercise_one.py
@@ -1,12 +1,19 @@
 #Toby Kerslake
-#12-09-2014
+#16-09-2014
 #Variable Improvement Stretch and Challenge Excercise 1
 
-length = float(input("Please enter the length of your garden"))
-width = float(input("Please enter the width of your garden"))
+width = float(input("Please enter the width of your garden in metres"))
+length = float(input("Please enter the length of your garden in metres"))
 
 gardenArea = length * width
 
 gardenPerimeter = (length * 2) + (width * 2)
+
+gardenTurfing = gardenArea - gardenPerimeter
+
+gardenPrice = gardenTurfing * 10
+
+print("Your garden has an area of {0} metres and a {1} metre perimeter with a 1 metre border. The total cost to turf the garden is £{2}".format(gardenArea,gardenPerimeter,gardenPrice))
+
 
 

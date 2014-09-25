57d4f557a592f4973516dac9137fece092dc9063
kerslaket <44108@longroad.ac.uk>
2014-09-25 21:05:48 +0100

    python_school_basic_excercises_1

---
+++ python_school_basic_excercises_1.py
@@ -0,0 +1,16 @@
+#17-09-14
+#Toby Kerslake
+#python school basic excercises 1
+
+
+number1 = int(input("Please enter your first number"))
+number2 = int(input("Please enter your second number"))
+number3 = int(input("Please enter your third number"))
+number4 = int(input("Please enter your fourth number"))
+
+numberTotal = number1 + number2 +number3 + number4
+
+print("Your total is {0}.".format(numberTotal))
+
+
+            
